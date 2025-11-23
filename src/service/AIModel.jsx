59e22e63232672a6ce434bbda01dc352e717a3e1
yash-1104github <gyah1104@gmail.com>
2025-11-23 18:33:48 +0530

    chore: Update

--- src/service/AIModel.jsx
+++ src/service/AIModel.jsx
@@ -1,6 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const apiKey = import.meta.env.VITE_GOOGLE_GEMINI_MODEL_API_KEY;
+console.log("apikey", apiKey);
 
 const genAI = new GoogleGenerativeAI(apiKey);
 
