404d6ac40ed38b8d17f9bd736758c88055cf6582
yash-1104github <gyah1104@gmail.com>
2025-12-28 22:40:33 +0530

    Chore: add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,19 @@ import Viewtrip from "./view-trip/index.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import Mytrips from "./my-trip";
 import CommnityTrips from "./commuity-tips/index.jsx";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Landing} from "./page/Landing";
+import VideoPlayer from "./page/GeneratePage";
+import LoginPage from "./page/LoginPage";
+import ProtectedRoute from "@/utils/ProtectedRoute";
+import SignupPage from "./page/SignUpPage";
+import { ChatProvider } from "@/context/ChatContext";
+import { VideoProvider } from "@/context/VideoContext";
+import { Analytics } from "@vercel/analytics/react"
 
 const router = createBrowserRouter([
   {
@@ -41,6 +54,7 @@ window.initMap = () => {
 const RootComponent = () => {
   return (
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
+      <Analytics />
       <Headers />
       <RouterProvider router={router} />
       <Toaster />
